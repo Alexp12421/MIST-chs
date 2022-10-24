0f6c72895401a112f8aa61ed20ab1a7c0475663d
Alex <alexpalescy@gmail.com>
2022-10-24 23:01:14 +0300

    Firebase added and some tests with firebase's values from a title.

--- app/src/main/java/com/example/mist/MainActivity.java
+++ app/src/main/java/com/example/mist/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,12 +8,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     public TextView main_text;
     public Button buttonus;
+    public Titlu titlu;
+    private com.google.firebase.database.DatabaseReference databaseReference;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +36,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 main_text.setText("Shmeck");
+            }
+        });
+
+        databaseReference = database.getReference().child("Titluri");
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                titlu = snapshot.getValue(Titlu.class);
+                main_text.setText(titlu.getTitle());
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
