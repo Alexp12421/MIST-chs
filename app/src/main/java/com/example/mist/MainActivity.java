0087cb714252e0680397031551cd972a536d061d
Alex <alexpalescy@gmail.com>
2022-10-26 23:47:32 +0300

    Register page styling is done.

---
+++ app/src/main/java/com/example/mist/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.mist;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    public TextView main_text;
+    public Button registerButton;
+    public Titlu titlu;
+    private com.google.firebase.database.DatabaseReference databaseReference;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        main_text = (TextView) findViewById(R.id.main_text_id);
+
+        registerButton = (Button)findViewById(R.id.id_RegisterButton);
+
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openRegister();
+            }
+        });
+
+        databaseReference = database.getReference().child("Titluri");
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                titlu = snapshot.getValue(Titlu.class);
+                main_text.setText(titlu.getTitle());
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    public void openRegister(){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+}
