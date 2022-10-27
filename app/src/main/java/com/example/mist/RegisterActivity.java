6579201c36cecb9d1b9c73773b8d92986341a5b0
Alex <alexpalescy@gmail.com>
2022-10-28 00:03:37 +0300

    Register activity is now linked with firebase. Now we can insert accounts into the database.

--- app/src/main/java/com/example/mist/RegisterActivity.java
+++ app/src/main/java/com/example/mist/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.example.mist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,8 +10,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -100,7 +106,33 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
             return;
         }
 
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            User user = new User(username,email);
 
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful()){
+                                                Toast.makeText(RegisterActivity.this,"User has been registered successfully!",Toast.LENGTH_LONG).show();
+
+                                                //redirect to login!
+                                                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                                            }else{
+                                                Toast.makeText(RegisterActivity.this, "Failed to register! Try again!", Toast.LENGTH_LONG).show();
+                                            }
+                                        }
+                                    });
+                        }else{
+                            Toast.makeText(RegisterActivity.this, "Failed to register! Try again!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
 
 
     }
---
+++ app/src/main/java/com/example/mist/User.java
@@ -0,0 +1,16 @@
+package com.example.mist;
+
+public class User {
+
+    public String username, emial;
+
+    public User(){
+
+    }
+
+    public User(String username, String email){
+        this.username = username;
+        this.emial = email;
+    }
+
+}
