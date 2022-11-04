c01b4af2426a23206e0c7d77dfbd0775f3411b09
Alex <alexpalescy@gmail.com>
2022-11-04 15:12:38 +0200

    Navigation bar's style is ready. Functionality in progress.

--- app/src/main/java/com/example/mist/StoreActivity.java
+++ app/src/main/java/com/example/mist/StoreActivity.java
@@ -1,18 +1,45 @@
 package com.example.mist;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TextView;
 
 public class StoreActivity extends AppCompatActivity {
-
+    private DrawerLayout drawer;
+   // private TextView username;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
 
+       // username = findViewById(R.id.username_drawer);
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+
     }
 }
